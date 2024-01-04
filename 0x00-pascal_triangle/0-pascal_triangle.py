22d5fb05fb73df6b4443d4651e594abad94fa113
Salma Hussien <107444767+Sallmahussien@users.noreply.github.com>
2024-01-04 21:05:24 +0200

    Add task 0

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Define pascal_triangle function"""
+
+
+def pascal_triangle(n):
+    """Returns list of integer lists representing the Pascal's triangle"""
+
+    if n <= 0:
+        return []
+
+    result = [[1]]
+
+    for idx in range(n - 1):
+        current_row = result[idx]
+        current_row_length = len(current_row)
+
+        new_row = []
+
+        for idx2 in range(current_row_length - 1):
+            if (current_row[idx2 + 1]):
+                new_row.append(current_row[idx2] + current_row[idx2 + 1])
+
+        new_row = [1] + new_row + [1]
+        result.append(new_row)
+
+    return result
