035fe8210adf3e357e4b4fcecaf552201eccf69a
Salma Hussien <107444767+Sallmahussien@users.noreply.github.com>
2024-02-01 15:20:06 +0200

    Fix task 0

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -7,10 +7,12 @@ from typing import List
 def validUTF8(data: List[int]) -> bool:
     """Determines if a given data set represents a valid UTF-8 encoding."""
     for num in data:
-        if (num not in range(0, 128) and
-                num not in range(49280, 57280) and
-                num not in range(14712960, 15712192) and
-                num not in range(4034953344, 4156538816)):
+        binary_of_num = bin(num)[2:].zfill(8)
+
+        if (not binary_of_num.startswith('0') and
+                not binary_of_num.startswith('110') and
+                not binary_of_num.startswith('1110') and
+                not binary_of_num.startswith('11110')):
             return False
 
     return True
