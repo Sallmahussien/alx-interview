bf64dead4edd2f83e1362818dc1bfdcceb268be2
Salma Hussien <107444767+Sallmahussien@users.noreply.github.com>
2024-02-01 23:22:53 +0200

    Process the multi byte characters

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -6,25 +6,34 @@ from typing import List
 
 def validUTF8(data: List[int]) -> bool:
     """Determines if a given data set represents a valid UTF-8 encoding."""
-    for num in data:
-        if is_valid_utf8(num) == 0:
+    idx = 0
+    while idx < len(data):
+        if data[idx] <= 127:
+            idx += 1
+            continue
+
+        if data[idx] & 0b11110000 == 0b11110000:
+            idx = process_multi_byte(data, idx, 3)
+        elif data[idx] & 0b11100000 == 0b11100000:
+            idx = process_multi_byte(data, idx, 2)
+        elif data[idx] & 0b11000000 == 0b11000000:
+            idx = process_multi_byte(data, idx, 1)
+        else:
+            return False
+
+        if idx == -1:
             return False
 
     return True
 
 
-def is_valid_utf8(c):
-    if c <= 0x7F:
-        return 1
-    elif 0xC080 == c:
-        return 1
-    elif 0xC280 <= c <= 0xDFBF:
-        return ((c & 0xE0C0) == 0xC080)
-    elif 0xEDA080 <= c <= 0xEDBFBF:
-        return 0
-    elif 0xE0A080 <= c <= 0xEFBFBF:
-        return ((c & 0xF0C0C0) == 0xE08080)
-    elif 0xF0908080 <= c <= 0xF48FBFBF:
-        return ((c & 0xF8C0C0C0) == 0xF0808080)
-    else:
-        return 0
+def process_multi_byte(data: List[int], idx: int, remaining_bytes: int) -> int:
+    """Helper function to process multi byte characters"""
+    while remaining_bytes != 0:
+        idx += 1
+        remaining_bytes -= 1
+
+        if not bin(data[idx])[2:].zfill(8)[-8:].startswith('10'):
+            return -1
+
+    return idx + 1
